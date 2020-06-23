8fe719b2ac31d57a82ef3915cd14882cfec0b878
ritik-gupta <ritikg8126@gmail.com>
2020-06-23 18:06:21 +0530

    enrollment: switch to POST method

--- application/routes.py
+++ application/routes.py
@@ -20,9 +20,9 @@ def courses():
 def register():
     return render_template("register.html", register=True)
 
-@app.route("/enrollment")
+@app.route("/enrollment", methods=["GET", "POST"])
 def enrollment():
-    id1 = request.args.get('courseID')
-    title = request.args.get('title')
-    term = request.args.get('term')
+    id1 = request.form.get('courseID')
+    title = request.form.get('title')
+    term = request.form.get('term')
     return render_template("enrollment.html", data={"id":id1, "title":title, "term":term})
